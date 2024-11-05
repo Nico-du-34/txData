dedd5d56e424229eeba7a3c9596107a7d15b4587
Nico-du-34 <contact@nicolas-arnaud.fr>
2024-11-05 14:38:23 +0000

    Add mapping

---
+++ los_santos_horizon.base/resources/[addons]/[mapping]/molo_ammunation/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'molo modding'
+description 'Ammunation'
+version '1.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+data_file 'TIMECYCLEMOD_FILE' 'molo_ammunation_timecycle'
+
+files {
+    'molo_ammunation_timecycle',
+}
+
+dependency '/assetpacks'

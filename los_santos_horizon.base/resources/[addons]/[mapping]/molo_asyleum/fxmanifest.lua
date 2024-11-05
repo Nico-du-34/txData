959e8c7e7e6cba146e0020cbe010fd6c2d272fa9
Nico-du-34 <contact@nicolas-arnaud.fr>
2024-11-05 14:41:06 +0000

    Add mapping

---
+++ los_santos_horizon.base/resources/[addons]/[mapping]/molo_asyleum/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'molo modding'
+description 'Asyleum'
+version '1.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+data_file 'AUDIO_GAMEDATA' 'audio/molo_asyleum_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/molo_asyleum_door_game.dat'
+
+
+files {
+
+'audio/molo_asyleum_game.dat151.rel',
+'audio/molo_asyleum_door_game.dat151.rel'
+}
+
+client_scripts {
+    'npc.lua'
+}
+dependency '/assetpacks'

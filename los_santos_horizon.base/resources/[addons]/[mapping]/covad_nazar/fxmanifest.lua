f4a9ca61623d5bad2ea100382cbaf7259a707c50
Nico-du-34 <contact@nicolas-arnaud.fr>
2024-11-05 14:28:39 +0000

    Add mapping

---
+++ los_santos_horizon.base/resources/[addons]/[mapping]/covad_nazar/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'CoVadich & molo modding'
+description 'Nazar'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+data_file 'AUDIO_GAMEDATA' 'audio/cv_nazar_game.dat'
+
+
+files {
+
+'audio/cv_nazar_game.dat151.rel'
+}
+dependency '/assetpacks'

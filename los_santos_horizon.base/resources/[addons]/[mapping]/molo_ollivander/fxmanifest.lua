4807eda04d42eeab194e90d436a7c0b2e13e72b0
Nico-du-34 <contact@nicolas-arnaud.fr>
2024-11-05 14:51:55 +0000

    Add mapping

---
+++ los_santos_horizon.base/resources/[addons]/[mapping]/molo_ollivander/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+this_is_a_map "yes"
+this_is_a_map "yes"
+
+
+files {
+
+"interiorproxies.meta"
+}
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+data_file 'AUDIO_GAMEDATA' 'audio/molo_ollivander_door_game.dat'
+
+
+files {
+  'audio/molo_ollivander_door_game.dat151.rel',
+
+}
+dependency '/assetpacks'

149aced93ea9bbaae18bf1176cbe78acf47c6fb7
Nico-du-34 <contact@nicolas-arnaud.fr>
2024-11-05 14:55:32 +0000

    Add mapping

---
+++ los_santos_horizon.base/resources/[addons]/[mapping]/molo_townhall/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+this_is_a_map "yes"
+lua54 'yes'
+
+data_file 'AUDIO_GAMEDATA' 'audio/molo_gouv_door_game.dat' 
+
+
+files {
+  'audio/molo_gouv_door_game.dat151.rel'
+}
+
+
+
+client_scripts {
+    'npc.lua'
+}
+dependency '/assetpacks'

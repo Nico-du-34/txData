efbfeba9f5266cd8f34bfbad593c9a01af7d16c6
Nico-du-34 <contact@nicolas-arnaud.fr>
2024-11-05 14:48:10 +0000

    Add mapping

---
+++ los_santos_horizon.base/resources/[addons]/[mapping]/molo_lspdhq/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'bodacious'
+game 'gta5'
+this_is_a_map 'yes'
+lua54 'yes'
+
+
+
+data_file('DLC_ITYP_REQUEST')('stream/props/molo_lspdhq_props.ytyp')
+
+
+
+
+data_file 'SCENARIO_INFO_FILE' 'alta.ymt'
+data_file 'SCENARIO_POINTS_PSO_FILE' 'alta.ymt'
+data_file 'FIVEM_LOVES_YOU_A01627E3E5FC490A' 'alta.ymt'
+data_file 'FIVEM_LOVES_YOU_399915A5EEB45A2B' 'alta.ymt'
+data_file 'FIVEM_LOVES_YOU_A9E8B9F6EEB35186' 'alta.ymt'
+data_file 'FIVEM_LOVES_YOU_DB4D236636684A3F' 'alta.ymt'
+
+dependency '/assetpacks'
+
+data_file 'AUDIO_GAMEDATA' 'audio/molo_lspdhq_game.dat' 
+
+
+files {
+  'audio/molo_lspdhq_game.dat151.rel',
+"audioconfig/game.dat151.rel"
+}
+
+escrow_ignore {
+  'stream/exteriormodels/*'
+
+  }
+  
+ file "audio/game.dat151.rel"
+dependency '/assetpacks'

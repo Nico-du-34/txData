5041ea4b60f3b16249b069eb6c891c8441e76f01
Nico-du-34 <contact@nicolas-arnaud.fr>
2024-11-22 12:54:41 +0000

    Mapping

---
+++ los_santos_horizon.base/resources/[addons]/[mapping]/[paid]/rfc_pawn_shop/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+this_is_a_map 'yes'
+
+Author 'Rfc_Mapping'
+description 'Pawn Shop'
+
+version '2.0'
+
+data_file 'AUDIO_GAMEDATA' 'audio/pawn_shop_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/pawn_mlo_game.dat'
+
+files {
+    'audio/pawn_shop_game.dat151.rel',
+    'audio/pawn_mlo_game.dat151.rel'
+}
+
+dependency '/assetpacks'
+dependency '/assetpacks'
